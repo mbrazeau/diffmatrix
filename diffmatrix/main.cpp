9ece4caa677524769418ddd1b133dd44fa8adce7
Martin Brazeau <martin.brazeau@gmail.com>
2021-05-28 14:50:18 +0100

    Initial Commit

---
+++ diffmatrix/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  diffmatrix
+//
+//  Created by Martin Brazeau on 28/05/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
